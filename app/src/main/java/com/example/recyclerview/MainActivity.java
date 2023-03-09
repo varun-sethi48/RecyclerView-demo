1b3c0f3a913eda518206027e621f2d6824ad2b1a
varun-sethi48 <mrsethi48@gmail.com>
2023-03-09 19:18:32 +0530

    This is my 2ND Commit of my Recycler View

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -25,17 +25,18 @@ ArrayList<ContactModel> arrContacts = new ArrayList<>();
 
         arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"A","1234567890"));
         arrContacts.add(new ContactModel(R.drawable.d,"B","9876543210"));
-        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"ABC","1234567890"));
-        arrContacts.add(new ContactModel(R.drawable.d,"C","9876543210"));
-        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"ABC","1234567890"));
+        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"C","1234567890"));
         arrContacts.add(new ContactModel(R.drawable.d,"D","9876543210"));
-        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"ABC","1234567890"));
-        arrContacts.add(new ContactModel(R.drawable.d,"E","9876543210"));
-        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"ABC","1234567890"));
+        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"E","1234567890"));
         arrContacts.add(new ContactModel(R.drawable.d,"F","9876543210"));
-        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"ABC","1234567890"));
-        arrContacts.add(new ContactModel(R.drawable.d,"G","9876543210"));
-        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"ABC","1234567890"));
+        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"G","1234567890"));
+        arrContacts.add(new ContactModel(R.drawable.d,"H","9876543210"));
+        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"I","1234567890"));
+        arrContacts.add(new ContactModel(R.drawable.d,"J","9876543210"));
+        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"K","1234567890"));
+        arrContacts.add(new ContactModel(R.drawable.d,"L","9876543210"));
+        arrContacts.add(new ContactModel(R.drawable.contact_us_svg,"M","1234567890"));
+        arrContacts.add(new ContactModel(R.drawable.d,"N","9876543210"));
 
 
         RecyclerContactAdapter adapter = new RecyclerContactAdapter(this,arrContacts);
